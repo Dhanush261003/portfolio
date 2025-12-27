2e1f1770a0dbeb69c02c17899011f616f80bcedb
Dhanush261003 <dhanushsekar2610@gmail.com>
2025-12-27 22:08:50 +0530

    4th

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import './Contact.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+import mail_icon from '../../assets/mail_icon.svg'
+import location_icon from '../../assets/location_icon.svg'
+import call_icon from '../../assets/call_icon.svg'
+
+const Contact = () => {
+    return (
+        <div className="contact">
+            <div className="contact-title">
+                <h1>Get in touch</h1>
+                <img src={theme_pattern} alt="" />
+            </div>
+            <div className="contact-section">
+                <div className="contact-left">
+                    <h1>Let's talk</h1>
+                    <p>I'm currently availabe to take on new projects, so feel free to contact</p>
+                    <div className="contact-details">
+                        <div className="contact-detail">
+                            <img src={mail_icon} alt="" /><p>dhanushsekar2610@gmail.com</p>
+                        </div>
+                        <div className="contact-detail">
+                            <img src={call_icon} alt="" /><p>+91 812474699</p>
+                        </div>
+                        <div className="contact-detail">
+                            <img src={location_icon} alt="" /><p>Kanchipuram, Tamilnadu</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
