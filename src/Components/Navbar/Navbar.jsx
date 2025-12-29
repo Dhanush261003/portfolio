a0630db4ebd2345da2e4500481413942deeb28b6
Dhanush261003 <dhanushsekar2610@gmail.com>
2025-12-29 19:30:11 +0530

    13 mobile view

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -23,11 +23,11 @@ const Navbar = () => {
             <img src={menu_open} onClick={openMenu} alt="" className='nav-mob-open' />
             <ul ref={menuRef} className="nav-menu">
                 <img onClick={closeMenu} className='nav-mob-close' src={menu_close} alt="" />
-                <li><AnchorLink className='anchor-link' offset={50} href='#home'><p onClick={() => setMenu("home")}>Home</p></AnchorLink>{menu === "home" ? <img src={underline} alt='' /> : <></>}</li>
-                <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={() => setMenu("about")}>About Me</p></AnchorLink>{menu === "about" ? <img src={underline} alt='' /> : <></>}</li>
-                <li><AnchorLink className='anchor-link' offset={50} href='#services'><p onClick={() => setMenu("services")}>Services</p></AnchorLink>{menu === "services" ? <img src={underline} alt='' /> : <></>}</li>
-                <li><AnchorLink className='anchor-link' offset={50} href='#work'><p onClick={() => setMenu("work")}>Portfolio</p></AnchorLink>{menu === "work" ? <img src={underline} alt='' /> : <></>}</li>
-                <li><AnchorLink className='anchor-link' offset={50} href='#contact'><p onClick={() => setMenu("contact")}>Contact</p></AnchorLink>{menu === "contact" ? <img src={underline} alt='' /> : <></>}</li>
+                <li><AnchorLink className='anchor-link' offset={50} href='#home'><p onClick={() => { setMenu("home"); closeMenu(); }}>Home</p></AnchorLink>{menu === "home" ? <img src={underline} alt='' /> : <></>}</li>
+                <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={() => { setMenu("about"); closeMenu(); }}>About Me</p></AnchorLink>{menu === "about" ? <img src={underline} alt='' /> : <></>}</li>
+                <li><AnchorLink className='anchor-link' offset={50} href='#services'><p onClick={() => { setMenu("services"); closeMenu(); }}>Services</p></AnchorLink>{menu === "services" ? <img src={underline} alt='' /> : <></>}</li>
+                <li><AnchorLink className='anchor-link' offset={50} href='#work'><p onClick={() => { setMenu("work"); closeMenu(); }}>Portfolio</p></AnchorLink>{menu === "work" ? <img src={underline} alt='' /> : <></>}</li>
+                <li><AnchorLink className='anchor-link' offset={50} href='#contact'><p onClick={() => { setMenu("contact"); closeMenu(); }}>Contact</p></AnchorLink>{menu === "contact" ? <img src={underline} alt='' /> : <></>}</li>
             </ul>
             <div className="nav-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
         </div>
